82392697e5f8357c2846e34aafb8154c86111009
Pedro Silva <psilva@cern.ch>
2016-05-25 18:00:11 +0200

    adding RECO from EDM GEN file

--- scripts/submitProduction_pp.py
+++ scripts/submitProduction_pp.py
@@ -4,29 +4,9 @@ import optparse
 import commands
 import time
 
-#command line configuration
-usage = 'usage: %prog [options]'
-parser = optparse.OptionParser(usage)
-parser.add_option('-q', '--queue'      ,dest='queue'  ,help='batch queue'          ,default='')
-parser.add_option('-j', '--jobs'       ,dest='jobs'   ,help='number of jobs'       ,default=1,    type=int)
-parser.add_option('-n', '--nevts'      ,dest='nevts'  ,help='number of evetns/job' ,default=100,  type=int)
-parser.add_option('-c', '--cfg'        ,dest='cfg'    ,help='cfg for the GEN step' ,default='PYTHIA_5p02GeV_FULLSIM.py')
-parser.add_option('-p', '--proc'       ,dest='proc'   ,help='process to generate'  ,default='TTbar')
-parser.add_option('-o', '--output'     ,dest='output' ,help='output directory'     ,default='/store/cmst3/group/hintt/CMSSW_7_5_8_patch2/')
-(opt, args) = parser.parse_args()
-
-#prepare working directory
-cmsswBase=os.environ['CMSSW_BASE']
-workBase='%s/src/UserCode/TopFromHeavyIons' % cmsswBase
-jobsBase='%s/FARM%s'%(workBase,time.time())
-os.system('mkdir -p %s'%jobsBase)
-
-#prepare output
-opt.output='%s/%s/'%(opt.output,opt.proc)
-os.system('cmsMkdir %s/RECO'%opt.output)
-
-#loop over the required number of jobs
-for n in xrange(1,opt.jobs+1):
+"""
+"""
+def submitJob(n,cfg,options,outF,output,cmsswBase,workBase,jobsBase):
 
     #create a wrapper for standalone cmssw job
     scriptFile = open('%s/runJob_%d.sh'%(jobsBase,n), 'w')
@@ -34,9 +14,11 @@ for n in xrange(1,opt.jobs+1):
     scriptFile.write('cd %s/src\n'%cmsswBase)
     scriptFile.write('eval `scram r -sh`\n')
     scriptFile.write('cd -\n')
-    scriptFile.write('cmsRun %s/test/%s maxEvents=%d hardProc=%s jobSeed=%s\n' % (workBase,opt.cfg,opt.nevts,opt.proc,n) )
-    scriptFile.write('xrdcp  -f Events_%d.root root://eoscms//eos/cms/%s/RECO/Events_%d.root\n' % (n,opt.output,n) )
-    scriptFile.write('rm Events_%d.root\n' % n)
+    scriptFile.write('cmsRun %s/test/%s %s\n' % (workBase,cfg,options))
+    scriptFile.write('eos mkdir %s\n'%output)
+    scriptFile.write('eos mkdir %s/RECO\n'%output)
+    scriptFile.write('xrdcp -f %s root://eoscms//eos/cms/%s/RECO/%s\n' % (outF,output,outF))
+    scriptFile.write('rm %s\n'%outF)
     scriptFile.close()
 
     #preare to run it
@@ -50,3 +32,55 @@ for n in xrange(1,opt.jobs+1):
         print 'Job #%d will run remotely' % n
         os.system("bsub -q %s -R \"swp>1000 && pool>30000\" -J HISIM%d \'%s/runJob_%d.sh\'" % (opt.queue,n,jobsBase,n) )
     
+
+
+
+#command line configuration
+usage = 'usage: %prog [options]'
+parser = optparse.OptionParser(usage)
+parser.add_option('-q', '--queue'      ,dest='queue'  ,help='batch queue'          ,default='')
+parser.add_option('-j', '--jobs'       ,dest='jobs'   ,help='number of jobs'       ,default=1,    type=int)
+parser.add_option('-n', '--nevts'      ,dest='nevts'  ,help='number of evetns/job' ,default=100,  type=int)
+parser.add_option('-i', '--input'      ,dest='input'  ,help='input directory with GEN jobs' ,default=None,  type='string')
+parser.add_option('-c', '--cfg'        ,dest='cfg'    ,help='cfg to run step' ,default='PYTHIA_5p02GeV_FULLSIM.py')
+parser.add_option('-p', '--proc'       ,dest='proc'   ,help='process to generate'  ,default='TTbar')
+parser.add_option('-o', '--output'     ,dest='output' ,help='output directory'     ,default='/store/cmst3/group/hintt/CMSSW_7_5_8_patch2/')
+(opt, args) = parser.parse_args()
+
+#prepare working directory
+cmsswBase=os.environ['CMSSW_BASE']
+workBase='%s/src/UserCode/TopFromHeavyIons' % cmsswBase
+jobsBase='%s/FARM%s'%(workBase,time.time())
+os.system('mkdir -p %s'%jobsBase)
+
+if opt.input:
+    from subprocess import Popen, PIPE
+    eos_cmd = '/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select'
+    data = Popen([eos_cmd, 'ls', '/eos/cms/'+opt.input],stdout=PIPE)
+    out,err = data.communicate()
+    n=0
+    for line in out.split('\n'):
+        if '.root' in line:
+            n+=1
+            outF='Events_%d.root'%n
+            submitJob(n=n,
+                      cfg=opt.cfg,
+                      options='input=%s output=%s'%(opt.input + '/' + line,outF),
+                      outF=outF,
+                      output=opt.output,
+                      cmsswBase=cmsswBase,
+                      workBase=workBase,
+                      jobsBase=jobsBase)
+            
+else:
+    for n in xrange(1,opt.jobs+1):
+        outF='Events_%d.root'%n
+        submitJob(n=n,
+                  cfg=opt.cfg,
+                  options='maxEvents=%d hardProc=%s jobSeed=%s\n' % (opt.nevts,opt.proc,n),
+                  outF=outF,
+                  output=opt.output,
+                  cmsswBase=cmsswBase,
+                  workBase=workBase,
+                  jobsBase=jobsBase)
+        
---
+++ test/Source_5p02GeV_FULLSIM.py
@@ -0,0 +1,136 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: reco --beamspot Nominal5TeVpp2015Collision --mc --eventcontent RECO --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --datatier RECO --conditions 75X_mcRun2_asymptotic_ppAt5TeV_v3 --step SIM,DIGI,L1,DIGI2RAW,HLT:PRef,RAW2DIGI,L1Reco,RECO --no_exec --filein=test.root
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('HLTrigger.Configuration.HLT_PRef_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+#parse command line arguments
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing('analysis')
+options.register ('input',
+		  '',
+		  VarParsing.multiplicity.singleton,
+		  VarParsing.varType.string,
+		  "Input file")
+options.register ('output',
+		  'Events.root',
+		  VarParsing.multiplicity.singleton,
+		  VarParsing.varType.string,
+		  "output file")
+options.parseArguments()
+
+
+# Input source
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    fileNames = cms.untracked.vstring(options.input),
+    inputCommands = cms.untracked.vstring('keep *', 
+#        'drop *_genParticles_*_*', 
+#        'drop *_genParticlesForJets_*_*', 
+#        'drop *_kt4GenJets_*_*', 
+#        'drop *_kt6GenJets_*_*', 
+#        'drop *_iterativeCone5GenJets_*_*', 
+#        'drop *_ak4GenJets_*_*', 
+#        'drop *_ak7GenJets_*_*', 
+#        'drop *_ak8GenJets_*_*', 
+#        'drop *_ak4GenJetsNoNu_*_*', 
+#        'drop *_ak8GenJetsNoNu_*_*', 
+#        'drop *_genCandidatesForMET_*_*', 
+#        'drop *_genParticlesForMETAllVisible_*_*', 
+#        'drop *_genMetCalo_*_*', 
+#        'drop *_genMetCaloAndNonPrompt_*_*', 
+#        'drop *_genMetTrue_*_*', 
+#        'drop *_genMetIC5GenJs_*_*'
+                                          ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('reco nevts:1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string(options.output),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '75X_mcRun2_asymptotic_ppAt5TeV_v3', '')
+
+# Path and EndPath definitions
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.RECOoutput_step])
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforFullSim 
+
+#call to customisation function customizeHLTforFullSim imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforFullSim(process)
+
+# End of customisation functions
+
